14642ba86334ee493f7b4d199cdc210d186f452e
nancy <smithnancy@outlook.com>
2023-05-26 17:45:31 +1000

    dayjs applied  on chrip compoment to display relatie dates format

--- resources/js/Components/Chrip.vue
+++ resources/js/Components/Chrip.vue
@@ -1,4 +1,9 @@
 <script setup>
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
+
 defineProps(['chrip']);
 </script>
 
@@ -11,7 +16,7 @@ defineProps(['chrip']);
             <div class="flex justify-between items-center">
                 <div>
                     <span class="text-gray-800">{{ chrip.user.name }}</span>
-                    <small class="ml-2 text-sm text-gray-600">{{ new Date(chrip.created_at).toLocaleString() }}</small>
+                    <small class="ml-2 text-sm text-gray-600">{{ dayjs(chrip.created_at).fromNow() }}</small>
                 </div>
             </div>
             <p class="mt-4 text-lg text-gray-900">{{ chrip.message }}</p>
