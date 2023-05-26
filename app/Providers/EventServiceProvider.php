f87beb8f86e4ca178dd79ff869663f0e3199c71f
nancy <smithnancy@outlook.com>
2023-05-26 21:04:33 +1000

    Registering the event listenner on providers

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Providers;
-
+use App\Events\ChripCreated;
+use App\Listeners\SendChripCreatedNotification;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -17,6 +18,9 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        ChripCreated::class => [
+            SendChripCreatedNotification::class,
         ],
     ];
 
