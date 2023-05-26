0d174d01b21e341c5ea540e212dbe2ce98d228a6
nancy <smithnancy@outlook.com>
2023-05-26 17:34:15 +1000

    fixing belongs to user

--- app/Models/Chrip.php
+++ app/Models/Chrip.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\database\Eloquent\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Chrip extends Model
 {
